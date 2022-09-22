eb9d3b7d32f4de68fe3d1f08f7f1e56bbb025621
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2022-09-22 21:40:31 +0200

    initial commit

---
+++ components/About.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+export default function About({}: Props) {
+  return (
+    <div className="flex flex-col releative h-screen text-center md:text-left">
+      About
+    </div>
+  );
+}
